ae72cf7bad58b1b8cab5dbe8f21e7d2dcaec493b
dasmemes <kenamugg@gmail.com>
2016-07-28 22:20:21 +0100

    Added temp user reg and cleanup html

--- application/users/views.py
+++ application/users/views.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, flash, render_template, url_for, redirect
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Length
 
 from .models import User
-from application import flask_bcrypt
+from application import flask_bcrypt, db
 
 
 Users = Blueprint('users', __name__, template_folder='templates')
@@ -14,8 +14,34 @@ Users = Blueprint('users', __name__, template_folder='templates')
 
 class LoginForm(Form):
 
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired(), Length(min=6)])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+
+
+class RegistrationForm(Form):
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), ])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+
+
+@Users.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('users.account'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        login_user(user, remember=True)
+        flash("You are now logged in.")
+        return redirect(url_for('users.account'))
+
+    return render_template('users/register.html', form=form)
 
 
 @Users.route('/login', methods=['GET', 'POST'])
@@ -37,7 +63,7 @@ def login():
             return render_template('users/login.html', form=form)
 
         login_user(user, remember=True)
-        flash("Success! You are now logged in.")
+        flash("You are now logged in!")
         return redirect(url_for('users.account'))
 
     return render_template('users/login.html', form=form)
@@ -46,9 +72,11 @@ def login():
 @Users.route('/logout', methods=['GET'])
 def logout():
     logout_user()
-    return redirect('/')
+    flash("You have successfully logged out!")
+    return redirect(url_for('users.login'))
 
 
 @Users.route('/account', methods=['GET'])
+@login_required
 def account():
     return render_template('users/account.html')
